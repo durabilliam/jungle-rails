4a8361a0b9cbb4aef71c29f82e7ac5cf443f37c7
William Perry <durabilliam@gamil.com>
2021-06-01 14:14:56 -0230

    add rspec homepage test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,7 +11,8 @@ require 'rspec/rails'
 
 require "capybara/rails"
 require "capybara/rspec"
-require "capybara/poltergeist" # Add this line to require poltergeist
+require "capybara/poltergeist" 
+# Add this line to require poltergeist
 
 # Specs flagged with `js: true` will use Capybara's JS driver. Set
 # that JS driver to :poltergeist
